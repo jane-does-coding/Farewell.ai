6ee4c36326b67241f0e1430bf9229ad2d8058ea1
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2025-09-30 17:41:28 -0600

    starting routes, before routes

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@
 import { useState } from "react";
 import { IoIosArrowDown, IoIosArrowForward } from "react-icons/io";
 import { motion, AnimatePresence } from "framer-motion";
+import { IoIosLink } from "react-icons/io";
 
 export default function Home() {
-	const [openTimeline, setOpenTimeline] = useState(false);
+	const [openTimeline, setOpenTimeline] = useState(true);
 
 	const pageVariants = {
 		hidden: {},
@@ -41,8 +42,8 @@ export default function Home() {
 			initial="hidden"
 			animate="visible"
 		>
-			<h2 className="pixel-sport text-[10vh]">Your Dashboard</h2>
-			<div className="flex gap-[5vw]">
+			<h2 className="pixel-sport text-[10vh] mb-[2vh]">Your Dashboard</h2>
+			<div className="flex gap-[7vw]">
 				{/* Tasks */}
 				<motion.div
 					className="flex flex-col w-[30vw]"
@@ -53,9 +54,9 @@ export default function Home() {
 					</h2>
 					<motion.div variants={listVariants}>
 						{[
-							{ text: "Drink more water", done: false, bar: "green-500/60" },
+							{ text: "Drink more water", done: false },
 							{ text: "Wake up at 7am", done: false },
-							{ text: "Workout", done: true, bar: "yellow-300/60" },
+							{ text: "Workout", done: true },
 							{ text: "Meditate", done: false },
 						].map((habit, i) => (
 							<motion.div
@@ -63,11 +64,6 @@ export default function Home() {
 								variants={itemVariants}
 								className="flex items-center justify-between relative py-[0.5vh]"
 							>
-								{habit.bar && (
-									<div
-										className={`absolute top-[50%] left-[-1vw] translate-y-[-50%] w-[60%] z-[-1] bg-${habit.bar} h-[2vh]`}
-									></div>
-								)}
 								<h2 className="text-[2.25vh]">{habit.text}</h2>
 								<span
 									className={`h-[2vh] w-[2vh] rounded-[0.1vh] cursor-pointer border-black border-[2px] ${
@@ -77,6 +73,81 @@ export default function Home() {
 							</motion.div>
 						))}
 					</motion.div>
+					<h2 className="pixel-sport text-[6vh] leading-[6vh] mb-[1.75vh] mt-[5vh]">
+						Monthly Progression
+					</h2>
+					<div className="grid grid-cols-4 gap-y-[2vh] items-center justify-center">
+						<div className="flex flex-col items-center justify-center">
+							<h2 className="mb-[1vh] text-[2vh] font-extralight">Habit 1</h2>
+							<div className="h-[300px] bg-neutral-100 rounded-[0.5vh] overflow-hidden border-[1px] border-neutral-300 w-[25px] flex flex-col items-center justify-end">
+								<div className="bg-blue-300 w-full h-[30px]"></div>
+								<div className="bg-black w-full h-[10px]"></div>
+								<div className="bg-blue-300 w-full h-[10px]"></div>
+								<div className="bg-black w-full h-[10px]"></div>
+								<div className="bg-blue-300 w-full h-[70px]"></div>
+								<div className="bg-black w-full h-[20px]"></div>
+								<div className="bg-blue-300 w-full h-[50px]"></div>
+								<div className="bg-black w-full h-[10px]"></div>
+								<div className="bg-blue-300 w-full h-[30px]"></div>
+							</div>
+						</div>
+						<div className="flex flex-col items-center justify-center">
+							<h2 className="mb-[1vh] text-[2vh] font-extralight">Habit 1</h2>
+							<div className="h-[300px] bg-neutral-100 rounded-[0.5vh] overflow-hidden border-[1px] border-neutral-300 w-[25px] flex flex-col items-center justify-end">
+								<div className="bg-blue-300 w-full h-[30px]"></div>
+								<div className="bg-black w-full h-[10px]"></div>
+								<div className="bg-blue-300 w-full h-[10px]"></div>
+								<div className="bg-black w-full h-[10px]"></div>
+								<div className="bg-blue-300 w-full h-[70px]"></div>
+								<div className="bg-black w-full h-[20px]"></div>
+								<div className="bg-blue-300 w-full h-[50px]"></div>
+								<div className="bg-black w-full h-[10px]"></div>
+								<div className="bg-blue-300 w-full h-[30px]"></div>
+							</div>
+						</div>
+						<div className="flex flex-col items-center justify-center">
+							<h2 className="mb-[1vh] text-[2vh] font-extralight">Habit 1</h2>
+							<div className="h-[300px] bg-neutral-100 rounded-[0.5vh] overflow-hidden border-[1px] border-neutral-300 w-[25px] flex flex-col items-center justify-end">
+								<div className="bg-blue-300 w-full h-[30px]"></div>
+								<div className="bg-black w-full h-[10px]"></div>
+								<div className="bg-blue-300 w-full h-[10px]"></div>
+								<div className="bg-black w-full h-[10px]"></div>
+								<div className="bg-blue-300 w-full h-[70px]"></div>
+								<div className="bg-black w-full h-[20px]"></div>
+								<div className="bg-blue-300 w-full h-[50px]"></div>
+								<div className="bg-black w-full h-[10px]"></div>
+								<div className="bg-blue-300 w-full h-[30px]"></div>
+							</div>
+						</div>
+						<div className="flex flex-col items-center justify-center">
+							<h2 className="mb-[1vh] text-[2vh] font-extralight">Habit 1</h2>
+							<div className="h-[300px] bg-neutral-100 rounded-[0.5vh] overflow-hidden border-[1px] border-neutral-300 w-[25px] flex flex-col items-center justify-end">
+								<div className="bg-blue-300 w-full h-[30px]"></div>
+								<div className="bg-black w-full h-[10px]"></div>
+								<div className="bg-blue-300 w-full h-[10px]"></div>
+								<div className="bg-black w-full h-[10px]"></div>
+								<div className="bg-blue-300 w-full h-[70px]"></div>
+								<div className="bg-black w-full h-[20px]"></div>
+								<div className="bg-blue-300 w-full h-[50px]"></div>
+								<div className="bg-black w-full h-[10px]"></div>
+								<div className="bg-blue-300 w-full h-[30px]"></div>
+							</div>
+						</div>
+						<div className="flex flex-col items-center justify-center">
+							<h2 className="mb-[1vh] text-[2vh] font-extralight">Habit 1</h2>
+							<div className="h-[300px] bg-neutral-100 rounded-[0.5vh] overflow-hidden border-[1px] border-neutral-300 w-[25px] flex flex-col items-center justify-end">
+								<div className="bg-blue-300 w-full h-[30px]"></div>
+								<div className="bg-black w-full h-[10px]"></div>
+								<div className="bg-blue-300 w-full h-[10px]"></div>
+								<div className="bg-black w-full h-[10px]"></div>
+								<div className="bg-blue-300 w-full h-[70px]"></div>
+								<div className="bg-black w-full h-[20px]"></div>
+								<div className="bg-blue-300 w-full h-[50px]"></div>
+								<div className="bg-black w-full h-[10px]"></div>
+								<div className="bg-blue-300 w-full h-[30px]"></div>
+							</div>
+						</div>
+					</div>
 				</motion.div>
 
 				{/* Goals */}
@@ -93,7 +164,7 @@ export default function Home() {
 							<motion.div
 								key={i}
 								variants={listVariants}
-								className="flex items-start border-b-[1px] border-neutral-500 text-start flex-col justify-start py-[1.75vh] gap-[0.5vh] relative"
+								className="flex items-start border-b-[1px] border-neutral-500 text-start flex-col justify-start py-[2.5vh] pb-[3vh] gap-[0.5vh] relative"
 							>
 								{/* Goal Title */}
 								<motion.h2
@@ -181,6 +252,20 @@ export default function Home() {
 										</AnimatePresence>
 									</>
 								)}
+
+								{/* Links */}
+
+								<div className="flex gap-[0.5vw] mt-[1vh]">
+									<span className=" flex items-center justify-center gap-[0.5vw] rounded-full border-[1px] border-neutral-200 cursor-pointer px-[1vw] py-[0.25vh] text-[1.5vh] font-medium">
+										<IoIosLink className="text-[2vh]" /> Some Link
+									</span>
+									<span className=" flex items-center justify-center gap-[0.5vw] rounded-full border-[1px] border-neutral-200 cursor-pointer px-[1vw] py-[0.25vh] text-[1.5vh] font-medium">
+										<IoIosLink className="text-[2vh]" /> Some Link
+									</span>
+									<span className=" flex items-center justify-center gap-[0.5vw] rounded-full border-[1px] border-neutral-200 cursor-pointer px-[1vw] py-[0.25vh] text-[1.5vh] font-medium">
+										<IoIosLink className="text-[2vh]" /> Some Link
+									</span>
+								</div>
 
 								{/* Habits */}
 								<motion.div
